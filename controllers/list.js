7612f8c7ac92924831aae5d14c39c59fa29c40a8
sebastian Quiroga <sebasquirogasb@gmail.com>
2025-06-19 12:42:08 -0500

    Se agrega funciones de listado

--- controllers/list.js
+++ controllers/list.js
@@ -2,21 +2,43 @@ import { ModelLista } from '../Schemas/mongooseModels.js'
 // import mongoconection from '../Database/mongo.js'
 
 export async function insertar (req, res) {
-  const { idTienda, listaReal } = req.body
-  ModelLista.create({
-    idTienda,
-    listaReal
+  await ModelLista.findOne().then((data) => {
+    if (data.estadoLista === 1) { res.send('hay una lista en ejecucion, no se puede insertar otra lista') } else if (data.estadoLista === 2) { res.send('hay una lista pendiente de compra')/* crear funcion para permitir crear una nueva lista */ } else {
+      const { idTienda, listaReal } = req.body
+      ModelLista.create({
+        idTienda,
+        listaReal
+      })
+        .then((data) => {
+          res.status(201).json({
+            message: 'Lista insertada correctamente',
+            data
+          })
+        })
+        .catch((err) => {
+          res.status(500).json({
+            message: 'Error al insertar la lista',
+            error: err.message
+          })
+        })
+    }
   })
-    .then((data) => {
-      res.status(201).json({
-        message: 'Lista insertada correctamente',
-        data
-      })
-    })
+}
+
+export async function forzar () {
+  await ModelLista.create({
+    idTienda: 12,
+    listaReal: [{
+      productName: '1aa2',
+      calidadProduct: 'Buena',
+      providerName: 'Juan',
+      cantidadCompra: 1
+    }]
+
+  }).then((data) => {
+    console.log(data)
+  })
     .catch((err) => {
-      res.status(500).json({
-        message: 'Error al insertar la lista',
-        error: err.message
-      })
+      console.error('Error al actualizar el estado de las listas:', err.message)
     })
 }
--- routes/list.routes.js
+++ routes/list.routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
-import { insertar } from '../controllers/list.js'
+import { forzar, insertar } from '../controllers/list.js'
 
 export const listRoutes = Router()
 
 listRoutes.post('/insertar', insertar)
+listRoutes.post('/forzar', forzar)
